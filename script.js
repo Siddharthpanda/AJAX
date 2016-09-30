47a4fa4d93b5959e69b305820e82e5c0385275cf
siddharthpanda <siddharth.panda@navaratan.com>
2016-09-30 20:03:55 +0530

    Second

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
-var mybutton = document.getElementById("loadbutton");
-mybutton.onclick = LoadAjax;
-
-function LoadAjax() {
-//for(var i=0; i<100; i++){
-if (XMLHttpRequest) {    
-var req = new XMLHttpRequest(); }
-else {
-    req = new ActiveXObject("Microsoft.XMLHTTP");
-}
-    req.open('GET','data.json');
-    //req.send();
-    //console.log(req);
-    req.onreadystatechange =function(){
-    if(req.status===200 && req.readyState===4) {
-        //console.log(req);
-        //document.writeln(req.responseText);
-        //var modify = document.getElementById('update');
-        //var modify = document.getElementsByTagName("li");
-        //modify[2].innerHTML = req.responseText;
-        /*for(var i=0;i<modify.length;i++){
-          //  modify[i].innerHTML = req.responseText;
-        }  */
-        //console.log(req.responseXML.getElementsByTagName("name")[1].childNodes[0].nodeValue)
-        
-        var items = JSON.parse(req.responseText);
-        console.log(items);
-        var output ='<ul>';
-        for (var key in items){
-            output += '<li>' + items[key].color +'</li>';
-        }
+$('#search').keyup(function(){
+    var searchField = $('#search').val();
+    // console.log(searchField);
+    var myexp = RegExp(searchField,"i");
+    $.getJSON('data.json', function(data){
+       //console.log(data); 
+        var output = "<ul class='searchresults'>";
+        $.each(data, function(key, val){
+            if( ((val.color.search(myexp)) != -1) ||((val.value.search(myexp)) != -1) ) {
+                output += '<li>';
+                output += '<h2>' + val.color + '</h2>';
+                output += '</li>';
+            }
+        });
         output += '</ul>';
-        document.getElementById('update').innerHTML=output;
-    }
-    }
-    req.send();
-//}    
-}
+        $('#update').html(output);
+    });
+});
